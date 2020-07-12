606ef849d5e3b6b97402bc0742ef29dbf78038e7
Jarosław Niesyto (jaronie711) <jaronie711@student.polsl.pl>
2020-07-12 05:21:12 +0200

    test 12

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -2,17 +2,11 @@ import React from 'react';
 import './Button.css';
 
 export default function Button(props) {
+
     return (
-        /* 
-        <span className="ButtonContainer" onClick={props.onClick} data-testid="testButton">
-            <button className={"Button " + props.variant} >
-             */
-                <div className={props.variant === "Small" ? "ButtonText TextSmall" : "ButtonText"}>    
-                    props.children      
-                </div>
-                 /* 
-            </button>
-        </span>
-                      */
+        <div>
+            {props.children}
+        </div>
     );
+
 }
