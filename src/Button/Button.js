b1ba00a033a317a7a390cdfb2a8235da494cfc4b
Jarosław Niesyto (jaronie711) <jaronie711@student.polsl.pl>
2020-07-12 05:42:49 +0200

    Styling fix

--- src/Button/Button.js
+++ src/Button/Button.js
@@ -4,7 +4,7 @@ import './Button.css';
 export default function Button(props) {
 
     return (
-        <span className="ButtonContainer" onClick={props.onClick} data-testid="testButton">
+        <span className={"ButtonContainer " + props.variant} onClick={props.onClick} data-testid="testButton">
             <button className={"Button " + props.variant} >
                 <div className={props.variant === "Small" ? "ButtonText TextSmall" : "ButtonText"}>
                     {props.children}
